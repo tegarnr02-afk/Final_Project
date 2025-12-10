c3a2792b066bbea4e93e25971497e5b285eec4c2
tegarnr02-afk <tegarnr02@gmail.com>
2025-12-10 23:03:34 +0700

    app.py

--- app.py
+++ app.py
@@ -12,24 +12,6 @@ from sklearn.utils.extmath import softmax as sk_softmax
 
 st.set_page_config(page_title="Amazon Review Sentiment", layout="wide")
 
-col_left, col_right = st.columns([1,9])
-with col_left:
-    checked = st.session_state["theme"] == "light"
-
-    toggle_html = f"""
-    <label class="switch">
-      <input type="checkbox" {'checked' if checked else ''} onchange="fetch('?toggle=1')">
-      <span class="slider"></span>
-    </label>
-    """
-
-    st.markdown(toggle_html, unsafe_allow_html=True)
-
-# Script to catch toggle
-if "toggle" in st.query_params:
-    toggle_theme()
-    st.query_params.clear()   # reset param
-    st.rerun()
 
 
 def switch_theme():
@@ -77,23 +59,6 @@ textarea, input, .stTextArea textarea {
 .sub { color: #333333 !important; }
 </style>
 """
-# DARK vs LIGHT settings
-if st.session_state["theme"] == "dark":
-    bg = "#0f1720"
-    text = "#ffffff"
-    input_bg = "#1a1f27"
-    input_text = "#ffffff"
-    btn_bg = "#11131a"
-    btn_text = "#ffffff"
-else:
-    bg = "#ffffff"
-    text = "#000000"
-    input_bg = "#f0f0f0"
-    input_text = "#000000"
-    btn_bg = "#e6e6e6"
-    btn_text = "#000000"
-
-st.markdown(theme_css % (bg, text, input_bg, input_text, btn_bg, btn_text), unsafe_allow_html=True)
 
 
 if st.session_state.theme == "dark":
@@ -111,27 +76,6 @@ with colD2:
 
 
 # CSS untuk tema
-if st.session_state["theme"] == "dark":
-    st.markdown("""
-        <style>
-        .stApp { background-color:#0f1720; color:#e6eef8; }
-        textarea, input, .stTextInput > div > div > input {
-            background-color:#11131a !important;
-            color:white !important;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-
-else:  # LIGHT MODE
-    st.markdown("""
-        <style>
-        .stApp { background-color:white; color:black; }
-        textarea, input, .stTextInput > div > div > input {
-            background-color:#f5f5f5 !important;
-            color:black !important;
-        }
-        </style>
-    """, unsafe_allow_html=True)
 
 # --- Style ---
 st.markdown(
